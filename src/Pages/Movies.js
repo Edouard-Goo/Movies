97684c104a4e4272fe7bb6b647b481c5caa7bc18
Edouard-Goo <edouard.gooda@gmail.com>
2023-09-10 10:17:00 +0200

    updated  Movies

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -3,6 +3,16 @@ import Movie from "../Components/Movie";
 
 const Movies = () => {
   const [data, setData] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [btn, setBtn] = useState();
+
+  const theClick = () => {
+    if (setBtn) {
+      setPages(pages + 1);
+    }
+
+    console.log("on y est");
+  };
 
   useEffect(() => {
     const options = {
@@ -18,7 +28,7 @@ const Movies = () => {
     // ATTENTION AUX BOUCLES INFINIES MEME SI CA FONCTIONNE
 
     fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=3",
+      `https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=${pages}`,
       options
     )
       .then((response) => response.json())
@@ -28,13 +38,17 @@ const Movies = () => {
         console.log("Test pour récupération de la Data ");
       })
       .catch((err) => console.error(err));
-  }, []);
+  }, [pages]);
 
   return (
     <>
       <div>
         <Movie liste={data} />
       </div>
+
+      <button type="submit" onClick={theClick}>
+        NEXT
+      </button>
     </>
   );
 };
